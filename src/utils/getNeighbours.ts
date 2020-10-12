b22f3d3c19ca400d147b6a63a1fb9d8d5dd7c95d
Nabi Isakhanov <nabi.isakhanov@dataart.com>
2020-10-13 01:32:53 +0300

    Fix bug with wrong calculations of neighbours

--- src/utils/getNeighbours.ts
+++ src/utils/getNeighbours.ts
@@ -3,8 +3,8 @@ import { isNil } from "lodash";
 export function getNeighbours(cellX: number, cellY: number, field: number[][]): number[] {
     const neighbours: number[] = [];
 
-    for (let y = cellY - 1; y < cellY + 1; y++) {
-        for (let x = cellX - 1; x < cellX + 1; x++) {
+    for (let y = cellY - 1; y <= cellY + 1; y++) {
+        for (let x = cellX - 1; x <= cellX + 1; x++) {
             if (x == cellX && y == cellY) continue;
             if (isNil(field[y]?.[x])) continue;
             neighbours.push(field[y][x]);
